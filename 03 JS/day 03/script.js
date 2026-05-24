5b84ca9022d7c7a47fe50d9f46918be8ea002a87
Jayant Kushwah <jayantkushwah8@gmail.com>
2026-05-24 21:35:17 +0530

    JavaScript day 3

---
+++ 03 JS/day 03/script.js
@@ -0,0 +1,11 @@
+let array = [1, 5, 8, 3, 6, 9, 3];
+for (let index = 0; index < array.length; index++) {
+  console.log(array[index]);
+}
+
+let i = 0;
+while (i < 10) {
+  console.log("i am the greatest");
+  if (i == 5) continue;
+  if (i == 9) break;
+}
